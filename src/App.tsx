9b6e91e49ec8a4cbb5ff861ef89c491fb881f046
Victoria Shevchenko <shevviktoryia@gmail.com>
2025-01-24 22:07:30 +0300

    feat: add mobile-app section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { HeroSection } from "./components/mainBaner/HeroSection";
 import { FavouriteCoffee } from "./components/FavouriteCoffee/FavouriteCoffee";
 import { About } from "./components/About/About";
+import { MobileApp } from "./components/MobileApp/MobileApp";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <HeroSection />
       <FavouriteCoffee />
       <About />
+      <MobileApp />
     </div>
   );
 }
---
+++ src/components/MobileApp/MobileApp.tsx
@@ -0,0 +1,40 @@
+import "./Mobile.css";
+import apple from "../../assets/svg/apple.svg";
+import play from "../../assets/svg/play.svg";
+
+export const MobileApp = () => {
+  return (
+    <section className="mobile-app">
+      <div className="container">
+        <div className="mobile-app-content">
+          <div className="mobile-text">
+            <h2>
+              <span>Download</span> our apps to start ordering
+            </h2>
+            <p>
+              Download the Resource app today and experience the comfort of
+              ordering your favorite coffee from wherever you are
+            </p>
+            <div className="apps-btns">
+              <a href="#" className="app-btn">
+                <img src={apple} alt="apple" />
+                <div className="text_btn">
+                  <p>Available on the</p>
+                  <p>App Store</p>
+                </div>
+              </a>
+              <a href="#" className="app-btn">
+                <img src={play} alt="play" />
+                <div className="text_btn">
+                  <p>Available on</p>
+                  <p>Google Play</p>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div className="mobile-img"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
