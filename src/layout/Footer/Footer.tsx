31d8a84fd2e0a1816bb04cf6fdf0a4b0d4ed8882
Victoria Shevchenko <shevviktoryia@gmail.com>
2025-01-25 23:50:25 +0300

    feat: add footer

---
+++ src/layout/Footer/Footer.tsx
@@ -0,0 +1,28 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer_content">
+          <div className="footer_left">
+            <h2>
+              Sip, Savor, Smile. <span> It’s coffee time!</span>
+            </h2>
+            <ul className="socials">
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          <div className="footer_right">
+            <h3>Contact us</h3>
+            <ul>
+              <li>8558 Green Rd., LA</li>
+              <li>+1 (603) 555-0123</li>
+              <li>Mon-Sat: 9:00 AM – 23:00 PM</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
